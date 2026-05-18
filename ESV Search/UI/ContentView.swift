8235de6a0b730c72fa3345c9eb17296bca951bc7
Jeff Mossberg <jmossberg@mac.com>
2026-05-17 22:42:05 -0700

    Final Opportunity
    This concludes working through the programming challenge. It is “as is”.

--- ESV Search/UI/ContentView.swift
+++ ESV Search/UI/ContentView.swift
@@ -1,13 +1,12 @@
 import SwiftUI
-
+import CoreData
 
 struct ContentView: View {
     @State var queryText = ""
     @State var queryActivated = false
     @State var searchResults: [SearchResult] = []
     @Environment(\.managedObjectContext) private var context
-
-
+            
     var body: some View {
         
         VStack {
@@ -21,20 +20,26 @@ struct ContentView: View {
                 "Search",
                 text: $queryText
             )
+            .padding(EdgeInsets(top: 0, leading: 50, bottom: 0, trailing: 50))
             .onSubmit {
                 queryActivated = true
             }
             .task(id: queryActivated) {
                 queryActivated = false // reset search flag
                 
-                if queryText.count < 2 { // One character is not a suitable search value
+                // One character is not a suitable search value, so we won't start search with less than 2 characters. There are valid 2 letter searches, Eg Og
+                if queryText.count < 2 {
                     searchResults = [] // Entering an empty search string should clear results
                     return
                 }
                 
-                // Here, we should first determine if we have this query cached in local storage.
-                // If not, then fetch results from server
-                searchResults = await fetchMatchesFromServer(query: queryText)
+                // Here, we should first determine if we have this query cached in local storage (presently Core Data)
+                searchResults = await fetchSearchesInLocalContext(context, forQuery: queryText)
+                
+                if searchResults.isEmpty {
+                    // If no locally stored matches, then fetch results from server
+                    searchResults = await fetchMatchesFromServerIntoContext(context, forQuery: queryText)
+                }
             }
             
             Text("\(searchResults.count) matches found")
@@ -57,7 +62,27 @@ struct ContentView: View {
     }
 }
 
-func fetchMatchesFromServer(query text: String) async -> [SearchResult] {
+
+private func fetchSearchesInLocalContext(_ context: NSManagedObjectContext, forQuery text: String) async -> [SearchResult] {
+    let fetchRequest: NSFetchRequest<SearchResultEntity> = SearchResultEntity.fetchRequest()
+    var searchResult = [SearchResult]()
+    var aResult: SearchResult
+
+    do {
+        let matches = try context.fetch(fetchRequest)
+        for match in matches {
+            aResult = SearchResult(reference: match.reference ?? "missing reference", content: match.content ?? "missing content")
+            searchResult.append(aResult)
+        }
+    } catch {
+        print("Failed to match items: \(error)")
+    }
+    
+    return searchResult
+}
+
+
+private func fetchMatchesFromServerIntoContext(_ context: NSManagedObjectContext, forQuery text: String) async -> [SearchResult] {
                     
     /////////////////////////////////////////////////
     let result = await activeESVAPI.search(text)
@@ -66,7 +91,20 @@ func fetchMatchesFromServer(query text: String) async -> [SearchResult] {
     switch result {
     case .success(let response):
         if !response.results.isEmpty {
-            // We need to cache these search results locally, but for now...
+            // We need to cache these search results locally, so do it here...
+            for match in response.results {
+                let searchData = SearchResultEntity(context: context)
+                searchData.query = text
+                searchData.reference = match.reference
+                searchData.content = match.content
+                //searchData.createdAt = Date()
+            }
+            
+            do {
+                try context.save()
+            } catch {
+                // We could politely handle the error appropriately, but the failure makes the ultimate solution needing to fetch again from the server.
+            }
             return response.results
         }
     case .failure(let error):
@@ -74,6 +112,7 @@ func fetchMatchesFromServer(query text: String) async -> [SearchResult] {
     }
     return []
 }
+
 
 #Preview(traits: .modifier(PersistencePreviewModifier())) {
     ContentView(searchResults: SearchResult.mockedData)
