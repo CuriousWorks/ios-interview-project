6152780827f0314670305fed2caea1b70da17250
Jeff Mossberg <jmossberg@mac.com>
2026-05-21 14:49:32 -0700

    Refactor Task Support
    Mostly just reorganizing things, and a few changes for clarity. Added a bit of documenation.

--- ESV Search/UI/ContentView.swift
+++ ESV Search/UI/ContentView.swift
@@ -8,7 +8,66 @@ struct ContentView: View {
     @State var dataSource = DataSource.server
     @State var statusMessage = "Pending"
     @Environment(\.managedObjectContext) private var context
+ 
+    // Retrieve matches from the local device
+    private func fetchSearchesInLocalContext(_ context: NSManagedObjectContext, forQuery searchString: String) async -> [SearchResult] {
+        let fetchRequest: NSFetchRequest<SearchResultEntity> = SearchResultEntity.fetchRequest()
+        var fetchResults = [SearchResult]()
+        var searchResult: SearchResult
+
+        do {
+            fetchRequest.predicate = NSPredicate(format: "query == %@", searchString)
+            fetchRequest.sortDescriptors = [NSSortDescriptor(key: "index", ascending: true)]
+            let matches = try context.fetch(fetchRequest)
+            for match in matches {
+                searchResult = SearchResult(reference: match.reference ?? "missing reference", content: match.content ?? "missing content")
+                fetchResults.append(searchResult)
+                match.lastSearch = Date()
+            }
+            try context.save() // Update the lastSearch date on device
+            dataSource = DataSource.device
+        } catch {
+            print("Failed to match items: \(error)")
+        }
+        
+        return fetchResults
+    }
+
+    // Retrieve matches from the server, and cache results locally on the device
+    private func fetchMatchesFromServerIntoContext(_ context: NSManagedObjectContext, forQuery searchString: String) async -> [SearchResult] {
+                        
+        /////////////////////////////////////////////////
+        let result = await activeESVAPI.search(searchString)
+        /////////////////////////////////////////////////
+        
+        switch result {
+        case .success(let response):
+            if !response.results.isEmpty {
+                // Cache search results locally
+                // Add index to preserve sequence (otherwise not sortable)
+                for (index, match) in response.results.enumerated() {
+                    let searchData = SearchResultEntity(context: context)
+                    searchData.index = Int16(index)
+                    searchData.query = searchString
+                    searchData.reference = match.reference
+                    searchData.content = match.content
+                    searchData.lastSearch = Date() // This can be used for a stale date to remove search results older than an arbitrary time, eg. older than 30 days.
+                }
                 
+                do {
+                    try context.save()
+                    dataSource = DataSource.server
+                } catch {
+                    // We could politely handle the error appropriately, but the failure makes the ultimate solution needing to fetch again from the server.
+                }
+                return response.results
+            }
+        case .failure(let error):
+            print(error)
+        }
+        return []
+    }
+
     var body: some View {
         VStack {
             // Banner
@@ -44,13 +103,11 @@ struct ContentView: View {
                 
                 // Here, first determine if this query cached in local storage (presently using Core Data)
                 searchResults = await fetchSearchesInLocalContext(context, forQuery: queryText)
-                dataSource = DataSource.device
                 
                 // If the query has not already been cached, the search results will be empty,
                 // and the server will be called upon for the search data.
                 if searchResults.isEmpty {
                     searchResults = await fetchMatchesFromServerIntoContext(context, forQuery: queryText)
-                    dataSource = DataSource.server
                 }
                 
                 isQueryActivated = false // Reset search flag so that the search field is ready for another search to be run.
@@ -84,64 +141,6 @@ struct ContentView: View {
         }
     }
 }
-
-
-private func fetchSearchesInLocalContext(_ context: NSManagedObjectContext, forQuery searchString: String) async -> [SearchResult] {
-    let fetchRequest: NSFetchRequest<SearchResultEntity> = SearchResultEntity.fetchRequest()
-    var fetchResults = [SearchResult]()
-    var searchResult: SearchResult
-
-    do {
-        fetchRequest.predicate = NSPredicate(format: "query == %@", searchString)
-        fetchRequest.sortDescriptors = [NSSortDescriptor(key: "index", ascending: true)]
-        let matches = try context.fetch(fetchRequest)
-        for match in matches {
-            searchResult = SearchResult(reference: match.reference ?? "missing reference", content: match.content ?? "missing content")
-            fetchResults.append(searchResult)
-            match.lastSearch = Date()
-        }
-        try context.save() // Update the lastSearch date on device
-    } catch {
-        print("Failed to match items: \(error)")
-    }
-    
-    return fetchResults
-}
-
-
-private func fetchMatchesFromServerIntoContext(_ context: NSManagedObjectContext, forQuery searchString: String) async -> [SearchResult] {
-                    
-    /////////////////////////////////////////////////
-    let result = await activeESVAPI.search(searchString)
-    /////////////////////////////////////////////////
-    
-    switch result {
-    case .success(let response):
-        if !response.results.isEmpty {
-            // Cache search results locally
-            // Add index to preserve sequence (otherwise not sortable)
-            for (index, match) in response.results.enumerated() {
-                let searchData = SearchResultEntity(context: context)
-                searchData.index = Int16(index)
-                searchData.query = searchString
-                searchData.reference = match.reference
-                searchData.content = match.content
-                searchData.lastSearch = Date()
-            }
-            
-            do {
-                try context.save()
-            } catch {
-                // We could politely handle the error appropriately, but the failure makes the ultimate solution needing to fetch again from the server.
-            }
-            return response.results
-        }
-    case .failure(let error):
-        print(error)
-    }
-    return []
-}
-
 
 #Preview(traits: .modifier(PersistencePreviewModifier())) {
     ContentView(searchResults: SearchResult.mockedData)
